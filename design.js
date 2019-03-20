8c552d32f53916db1b3c65ff0190bf89e3d50988
Reece387 <state2good@gmail.com>
2019-03-20 12:53:47 -0400

    Refactored snapping

--- design.js
+++ design.js
@@ -3,14 +3,6 @@ var fixedCardSet = false;
 let fixedCards = document.querySelectorAll(".fixed-card");
 var snapContainer = document.querySelector(".snap-scroll-container");
 
-fixedCards.forEach(function(fixedCard, i){
-    fixedCard.addEventListener("hover", function(){
-        window.requestAnimationFrame(function() {
-            fixedCard.classList.add("final");
-        });
-    });
-});
-
 snapContainer.addEventListener("scroll", function(e){
     fixedCards.forEach(function(card, i){
         if(card.matches(":hover")){
